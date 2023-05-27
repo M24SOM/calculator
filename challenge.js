7a77ef68e2555b1cf410ae5b30082252be2321b0
Mohamed Mahad Farah <mmfarah@proton.me>
2023-05-27 12:16:10 +0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,25 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculator(num1, num2, operator) {
+  let result;
+  if (operator === "+") {
+    result = num1 + num2;
+  } else if (operator === "-") {
+    result = num1 - num2;
+  } else if (operator === "*") {
+    result = num1 * num2;
+  } else if (operator === "/") {
+    result = num1 / num2;
+  } else {
+    console.log("Invalid operator");
+    return;
+  }
+  console.log(result);
+}
+
+calculator(5, 3, "+"); // Output: 8
+calculator(5, 3, "-"); // Output: 2
+calculator(5, 3, "*"); // Output: 15
+calculator(5, 3, "/"); // Output: 1.6666666666666667
+calculator(5, 3, "%"); // Output: Invalid operator
